18dd9c4be00c3ceb1db021bbcd09aaf0db8b01c6
z3301 <scoopnisker@gmail.com>
2023-10-10 15:49:27 -0400

    First and Final Commit

---
+++ ios101-lab5-flix1/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  ios101-lab5-flix1
+//
+//  Created by Daniel Zimmerman on 10/10/23.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
